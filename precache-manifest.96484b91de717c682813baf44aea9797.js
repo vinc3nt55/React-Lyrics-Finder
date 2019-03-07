bc69a5934dd293fc739633e38d61aafb80a6f046
vinc3nt55 <vinc3nt55@gmail.com>
2019-03-07 12:55:18 +0800

    Updates

---
+++ precache-manifest.96484b91de717c682813baf44aea9797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2943cf5a126a124ef44a",
+    "url": "/React-Lyric-Finder/static/js/runtime~main.2943cf5a.js"
+  },
+  {
+    "revision": "2b2b16d7e4133df9bb5e",
+    "url": "/React-Lyric-Finder/static/js/main.2b2b16d7.chunk.js"
+  },
+  {
+    "revision": "57bf4c21c9690616c822",
+    "url": "/React-Lyric-Finder/static/js/1.57bf4c21.chunk.js"
+  },
+  {
+    "revision": "2b2b16d7e4133df9bb5e",
+    "url": "/React-Lyric-Finder/static/css/main.ce73850b.chunk.css"
+  },
+  {
+    "revision": "99d74dd0b9f745927fe5baf4771157b5",
+    "url": "/React-Lyric-Finder/index.html"
+  }
+];
